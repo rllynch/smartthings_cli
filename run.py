29ccd8d5636df028fc95a3079b33d049d5982f65
Richard L. Lynch <rich@richlynch.com>
2016-01-04 23:14:48 -0500

    Fix convenience wrapper

--- run.py
+++ run.py
@@ -3,7 +3,7 @@
 
 '''Convenience wrapper for running smartthings_cli directly from source tree.'''
 
-from smartthings_cli.smartthings_cli. import main
+from smartthings_cli.smartthings_cli import main
 
 if __name__ == '__main__':
     main()
